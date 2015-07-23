14228d65dbb7f4d500b34a13890540f1093ec615
eugsong <eug.song@gmail.com>
2015-07-22 17:56:19 -0700

    partially done with figuring out OpenGL. Continue later.

--- OpenGLTest/main.cpp
+++ OpenGLTest/main.cpp
@@ -1,7 +1,16 @@
-#include "freeglut.h"
 #include "glew.h"
+#include "freeglut.h"
+#include <iostream>
+
 
 int main(int argc, char **argv)
 {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(500, 500);//optional
+	glutInitWindowSize(800, 600); //optional
+	glutCreateWindow("OpenGL First Window");
+
+	glutMainLoop();
 	return 0;
 }
